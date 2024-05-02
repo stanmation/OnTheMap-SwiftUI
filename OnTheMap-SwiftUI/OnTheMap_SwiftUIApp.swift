2a0353784ddb015c55863410fb88fb0addbed0a0
Stanley Darmawan <stanley.darmawan@lexicondigital.com.au>
2024-05-02 17:36:20 +1000

    Initial Commit

---
+++ OnTheMap-SwiftUI/OnTheMap_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnTheMap_SwiftUIApp.swift
+//  OnTheMap-SwiftUI
+//
+//  Created by Stanley Darmawan on 2/5/2024.
+//
+
+import SwiftUI
+
+@main
+struct OnTheMap_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
